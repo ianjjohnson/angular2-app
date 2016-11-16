040d07231bf4f1358a623a3c0f1226537063cf8c
Ian Johnson <ianjjohnson@icloud.com>
2016-11-16 09:25:03 -0600

    Still playing around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  title2= 'second title!';
+
+  enter(num){
+    if(num == 1){
+      this.title = "You hit Enter!";
+    } else {
+      this.title2= "You hit Enter2!";
+    }
+  }
 }
